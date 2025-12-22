4d9f1f9023c6399abb57482c34a9386602d33bba
Thangpt2004 <127660337+HadeZ04@users.noreply.github.com>
2025-12-22 20:38:51 +0700

    Delete check_football_matches.sql

--- check_football_matches.sql
+++
@@ -1,5 +0,0 @@
-USE ChampionLeagueManagement;
-GO
-SELECT COUNT(*) as FootballMatchCount FROM FootballMatches;
-SELECT TOP 5 utc_date, home_team_name, away_team_name FROM FootballMatches ORDER BY utc_date DESC;
-GO
