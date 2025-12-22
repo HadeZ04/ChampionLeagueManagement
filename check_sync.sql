0de014c28bb96adfaccb5985506db531f7739ea9
Thangpt2004 <127660337+HadeZ04@users.noreply.github.com>
2025-12-22 20:36:32 +0700

    Delete check_sync.sql

--- check_sync.sql
+++
@@ -1,6 +0,0 @@
-USE ChampionLeagueManagement;
-GO
-SELECT * FROM seasons ORDER BY start_date DESC;
-SELECT COUNT(*) as MatchCount FROM matches;
-SELECT TOP 5 * FROM matches ORDER BY match_id DESC;
-GO
